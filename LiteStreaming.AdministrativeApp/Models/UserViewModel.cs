af5e0c5b5c16363cd94af605bef7ab72b8813a50
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-07-02 00:49:41 -0300

    Delete UserViewModel.cs

--- LiteStreaming.AdministrativeApp/Models/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Application.Administrative.Dto;
-
-namespace LiteStreaming.AdministrativeApp.Models;
-public class UserViewModel
-{
-   public AdminAccountDto? Accout { get; set; }
-   public IEnumerable<AdminAccountDto>? Items { get; set; }
-   public PagerModel? PageModel { get; set; }
-}
